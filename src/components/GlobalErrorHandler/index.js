9e2fa2346958228f64c46e145bbc292d277bc650
Luis Felipe <luis.santos@devsquad.com>
2024-09-19 15:00:48 -0300

    implementar globalerror

--- src/components/GlobalErrorHandler/index.js
+++ src/components/GlobalErrorHandler/index.js
@@ -1,27 +1,14 @@
 import React, { useEffect } from 'react';
-
-import Debugmate from "debugmate"; // Caminho para o arquivo debugmate.js
+import Debugmate from 'debugmate';
 
 const debugmate = new Debugmate();
 
-const GlobalErrorHandler = ({children }) => {
+const GlobalErrorHandler = ({ children }) => {
     useEffect(() => {
-        const handleGlobalError = (message, source, lineno, colno, error) => {
-            console.error('Global error captured:', { message, source, lineno, colno, error });
-            debugmate.publish(error, { message, source, lineno, colno, error });
-        };
-
-        const handleUnhandledRejection = (event) => {
-            console.error('Unhandled Promise Rejection:', event.reason);
-            debugmate.publish(event.reason, { message: event.reason.message });
-        };
-
-        window.onerror = handleGlobalError;
-        window.addEventListener('unhandledrejection', handleUnhandledRejection);
+        debugmate.setupGlobalErrorHandling();
 
         return () => {
-            window.onerror = null;
-            window.removeEventListener('unhandledrejection', handleUnhandledRejection);
+            debugmate.cleanupGlobalErrorHandling();
         };
     }, []);
 
--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 import ErrorBoundary from './components/ErrorBoundary';
 import GlobalErrorHandler from './components/GlobalErrorHandler';
+import Debugmate from 'debugmate';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
